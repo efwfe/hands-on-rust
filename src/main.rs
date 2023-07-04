14083ad0e83e7d2547bb5b615f769e135fcaa6d7
David <david@laien.io>
2023-07-04 16:06:33 +0800

    add GameMode arms

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use bracket_lib::prelude::*;
 
 // Game state
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State{
+    fn new()->Self{
+        State { mode: GameMode::Menu }
+    }
+}
 
 // Game Modes
 enum GameMode{
@@ -12,15 +20,62 @@ enum GameMode{
 
 impl GameState for State{
     fn tick(&mut self, ctx: &mut BTerm){
-        ctx.cls();
-        ctx.print(1, 1, "Hello World!");
+        match self.mode{
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx ),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
+
+impl State{
+    // Play function stub
+    fn play(&mut self, ctx: &mut BTerm){
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self){
+        self.mode = GameMode::Playing;
+        
+    }
+
+    // Main Menu
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key{
+            match key{
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ =>{}
+            }
+        }
+    }
+
+    // Game Over 
+    fn dead(&mut self, ctx:&mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+        if let Some(key) = ctx.key{
+            match key{
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ =>{}
+            }
+        }
+    }
+}
+
 fn main() -> BError {
    let context = BTermBuilder::simple80x50()
     .with_title("Flappy Dragon")
     .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 
 }
