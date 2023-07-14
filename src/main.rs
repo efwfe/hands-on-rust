e3496bfc7d4a4f51fc22c8d7bd44e4cdbd5c58bb
David <david@laien.io>
2023-07-14 10:49:28 +0800

    add adventurer

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,41 @@ mod map;
 // imports map module into the global scope and set up the map::prefix
 // within main.rs items declared in map may be accessed as map::my_function
 
+mod player;
+
 mod prelude{
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32  = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     //  pub use super::map::*;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
 struct State{
     map: Map,
+    player: Player,
 }
 
 impl State{
     fn new() -> Self{
-        Self{map: Map::new()}
+        Self{
+            map: Map::new(), 
+            player: Player::new(
+                Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+            ),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
--- src/map.rs
+++ src/map.rs
@@ -27,7 +27,6 @@ pub enum TileType{
 
 
 // Create an empty Map
-
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
@@ -36,6 +35,22 @@ impl Map{
     pub fn new() ->Self{
         Self {
              tiles: vec![TileType::Floor; NUM_TILES]
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool{
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >=0 &&point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool{
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point)->Option<usize> {
+        if !self.in_bounds(point){
+            None
+        }else{
+            Some(map_idx(point.x, point.y))
         }
     }
 
